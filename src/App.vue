65efd2dada8451715bcf6690bae4458050243978
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-22 11:03:35 +0200

    Template Carousel.vue

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,15 @@
             <carousel-slide>
                 Salut les gens
             </carousel-slide>
+            <carousel-slide>
+                Aurevoir les gens
+            </carousel-slide>
         </carousel>
     </div>
 </template>
 
 <script>
-    import Carousel from './components/Carousel'
+    import Carousel from './components/carousel/Carousel'
 
     export default {
         name: 'app',
