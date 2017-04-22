8e931b7bf72aa3e1bf45e3d1d3e70b686d7d4cc5
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2017-04-22 11:35:35 +0200

    Add image for carousel

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
         <carousel>
             <carousel-slide>
                 Salut les gens
+                <img src="http://lorempicsum.com/futurama/630/300/1">
             </carousel-slide>
             <carousel-slide>
                 Aurevoir les gens
+                <img src="http://lorempicsum.com/futurama/630/300/2">
             </carousel-slide>
         </carousel>
     </div>
